693b0d34c4d0ab86e7a366b76b20bfbb141a96d5
avsigaev <avsigaev@gmail.com>
2019-05-25 02:49:13 +0300

    add critical temp & force reboot

--- sonm-fan-control.sh
+++ sonm-fan-control.sh
@@ -19,29 +19,33 @@ cleanup() {
 
 
 if [[ "$(id -u)" != "0" ]]; then
-   echo "This script must be run as superuser"
+   echo "ERROR: This script must be run as superuser"
    exit 1
 fi
 
 if ! lspci -nm | grep '"\(0300\|0302\)" "10de"' 2>&1 >/dev/null; then
-    echo "No NVIDIA GPUs detected"
+    echo "ERROR: No NVIDIA GPUs detected"
     exit 0
 fi
 
-echo "Found ${CARDS_NUM} GPU(s) ; MIN ${MIN_TEMP}°C - ${MAX_TEMP}°C MAX ; Min fan speed ${MIN_FAN_SPEED} : Delay ${DELAY}s"
+echo "INFO: Found ${CARDS_NUM} GPU(s) ; MIN ${MIN_TEMP}°C - ${MAX_TEMP}°C MAX ; Min fan speed ${MIN_FAN_SPEED} : Delay ${DELAY}s"
 
 for ((i=0; i<$CARDS_NUM; i++)); do
 	nvidia-settings -a [gpu:$i]/GPUFanControlState=1 2>&1 1>/dev/null
 	if [ "$?" -ne 0 ]; then
-		exit 1;
+		exit 1
 	fi
 done
 
-echo "GPUFanControlState set to 1 for all cards"
+echo "INFO: GPUFanControlState set to 1 for all cards"
 
 while true; do
 	for ((i=0; i<$CARDS_NUM; i++)); do
 		GPU_TEMP=`nvidia-smi -i $i --query-gpu=temperature.gpu --format=csv,noheader`
+		if [[ $GPU_TEMP -gt $CRIT_TEMP ]]; then
+			echo "CRITICAL: GPU${i} exceeded critical temp, forcing reboot"
+			reboot
+		fi
 		if [[ $GPU_TEMP -lt $MIN_TEMP ]]; then
 			FAN_SPEED=$MIN_FAN_SPEED
 		elif [[ $GPU_TEMP -gt $MAX_TEMP ]]; then
@@ -50,7 +54,11 @@ while true; do
 			FAN_SPEED=$(( $MIN_FAN_SPEED + ($GPU_TEMP - $MIN_TEMP)*(100 - $MIN_FAN_SPEED)/($MAX_TEMP - $MIN_TEMP) ))
 		fi
 		nvidia-settings -a [fan:$i]/GPUTargetFanSpeed=$FAN_SPEED 2>&1 1>/dev/null
-		echo "GPU${i} ${GPU_TEMP}°C, fan -> ${FAN_SPEED}%"
+		if [ "$?" -ne 0 ]; then
+			echo "ERROR: GPU${i} - Cannot set fan speed to ${FAN_SPEED}% (GPU temp is ${GPU_TEMP}°C)"
+		else
+			echo "INFO: GPU${i} ${GPU_TEMP}°C, fan -> ${FAN_SPEED}%"
+		fi
 	done
 	sleep $DELAY
 done
